422ccd47e564a2bd95aa3ff949c4fe713e28620a
Kevin Krause <kkrause@appirio.com>
2019-05-17 10:24:46 -0400

    Create files and JAR

---
+++ aws-lambda/AWS.java
@@ -0,0 +1,9 @@
+public class AWS {
+  public static void main(String[] args) {
+    System.out.println("Hello world!!!");
+  }
+
+  public void helloFromLambda() {
+    System.out.println("Hello, from AWS Lambda!");
+  }
+}
